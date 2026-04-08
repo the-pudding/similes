28f1dce186e8f0832eb3d83fb8b3fd884815f692
Russell Samora <russellsamora@gmail.com>
2026-04-08 15:28:43 -0400

    latest

---
+++ src/components/Explainer.svelte
@@ -0,0 +1,63 @@
+<script>
+	let { terms, example } = $props();
+</script>
+
+<div class="c">
+	<ul>
+		{#each terms as { term, description }}
+			<li><span>{term}</span> &rarr; {description}</li>
+		{/each}
+	</ul>
+
+	<p>{@html example}</p>
+</div>
+
+<style>
+	.c {
+		font-family: var(--font-sans);
+	}
+
+	ul {
+		list-style-type: none;
+		padding-left: 0;
+	}
+
+	p {
+		font-size: var(--24px);
+		font-weight: bold;
+	}
+
+	p :global(span) {
+		text-decoration: underline;
+		text-decoration-thickness: 4px;
+		text-underline-offset: 4px;
+		text-decoration-skip-ink: none;
+	}
+
+	p :global(span:nth-of-type(1)) {
+		text-decoration-color: var(--color-gold);
+	}
+
+	p :global(span:nth-of-type(2)) {
+		text-decoration-color: var(--color-green);
+	}
+
+	p :global(span:nth-of-type(3)) {
+		text-decoration-color: var(--color-purple);
+	}
+
+	li:nth-of-type(1) span {
+		color: var(--color-gold);
+		font-weight: bold;
+	}
+
+	li:nth-of-type(2) span {
+		color: var(--color-green);
+		font-weight: bold;
+	}
+
+	li:nth-of-type(3) span {
+		color: var(--color-purple);
+		font-weight: bold;
+	}
+</style>
--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -4,6 +4,7 @@
 	import Footer from "$components/Footer.svelte";
 	import Game from "$components/Game.svelte";
 	import ShapeExample from "$components/ShapeExample.svelte";
+	import Explainer from "$components/Explainer.svelte";
 	import loadCsv from "$utils/loadCsv.js";
 	import { chartData } from "$runes/misc.svelte.js";
 	import * as d3 from "d3";
@@ -12,7 +13,7 @@
 	// const data = getContext("data");
 
 	const body = copy?.body || [];
-	const components = { Game, ShapeExample };
+	const components = { Game, ShapeExample, Explainer };
 
 	$effect(async () => {
 		const raw = await loadCsv("assets/pairs.csv");
