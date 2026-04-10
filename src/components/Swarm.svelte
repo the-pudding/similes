3d33b500f449d219f9e5e1f9526eaacc9bd6d183
Russell Samora <russellsamora@gmail.com>
2026-04-10 16:15:50 -0400

    label

--- src/components/Swarm.svelte
+++ src/components/Swarm.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Plot, Dot } from "svelteplot";
+	import { Plot, Dot, Text } from "svelteplot";
 	import { chartData } from "$runes/misc.svelte.js";
 
 	const x = "score";
@@ -9,8 +9,30 @@
 </script>
 
 <div class="c graphic">
-	<Plot height={400} inset={20} r={{ range: [2, 16] }} y={{ axis: false }}>
-		<Dot {data} {x} sort={{ channel: "-r" }} y={0} {r} dodgeY="middle" />
+	<Plot
+		height={600}
+		inset={24}
+		r={{ range: [2, 32] }}
+		y={{ axis: false }}
+		x={[0, 1]}
+	>
+		<Dot
+			{data}
+			{x}
+			sort={{ channel: "-r" }}
+			y={0}
+			{r}
+			dodgeY={{ anchor: "middle", padding: 3 }}
+		/>
+		<Text
+			{data}
+			{x}
+			sort={{ channel: "-r" }}
+			y={0}
+			{r}
+			text={(d) => d.vehicle}
+			dodgeY={{ anchor: "middle", padding: 3 }}
+		/>
 	</Plot>
 </div>
 
