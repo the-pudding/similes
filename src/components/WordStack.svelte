e81b8d3bf262776b70637e9becdd113c893b12c7
Russell Samora <russellsamora@gmail.com>
2026-04-14 14:34:05 -0400

    all charts prototyped

--- src/components/WordStack.svelte
+++ src/components/WordStack.svelte
@@ -8,7 +8,7 @@
 			<div class="category"><strong>{category}</strong></div>
 			<div class="values">
 				{#each values as { ground, rank }}
-					<div class="value">{ground} <span>(#{rank})</span></div>
+					<div class="ground">{ground} <span>#{rank}</span></div>
 				{/each}
 			</div>
 		</div>
@@ -28,8 +28,28 @@
 		text-align: center;
 		display: flex;
 		flex-direction: column;
+		gap: 0.25rem;
 	}
 
-	.category {
+	.values {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 0.25rem;
+	}
+
+	.ground {
+		position: relative;
+		line-height: 1;
+		display: inline-block;
+	}
+
+	.ground span {
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		transform: translateX(calc(100% + 0.5em));
+		color: var(--color-gray-500);
+		font-size: 0.75em;
 	}
 </style>
