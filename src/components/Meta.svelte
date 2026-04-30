43aa1e9e0440ecfa8a128486cfe4101aafc0196c
Russell Samora <russellsamora@gmail.com>
2026-04-30 08:19:23 -0400

    og

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -21,8 +21,8 @@
 	<meta property="og:type" content="article" />
 	<meta property="og:locale" content="en_US" />
 
-	<meta property="og:image" content="{url}/assets/social-facebook.jpg" />
-	<meta property="og:image:type" content="image/jpeg" />
+	<meta property="og:image" content="{url}/assets/social.png" />
+	<meta property="og:image:type" content="image/png" />
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="628" />
 
@@ -31,7 +31,7 @@
 	<meta name="twitter:creator" content="@puddingviz" />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image:src" content="{url}/assets/social-twitter.jpg" />
+	<meta name="twitter:image:src" content="{url}/assets/social.png" />
 
 	<meta name="robots" content="max-image-preview:large" />
 
